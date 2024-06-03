b8776bad200913dd9ae4df46537498deb80d6579
AdanVolken <rolfivolken9@gmail.com>
2024-06-03 18:08:52 -0300

    Create SintomaViewModel

--- ViewModel/VMSintoma.cs
+++ ViewModel/VMSintoma.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using KineMovil.Commands;
 
 namespace KineMovil.ViewModel
 {
-    class VMSintoma
+    class VMSintoma  : INotifyPropertyChanged
     {
+        private string _nombre;
+        private int _idParte;
+        private string _descri;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; OnPropertyChanged(); }
+        }
+
+        public int IdParte
+        {
+            get { return _idParte; }
+            set { _idParte = value; OnPropertyChanged(); }
+        }
+
+        public string Descri
+        {
+            get { return _descri; }
+            set { _descri = value; OnPropertyChanged(); }
+        }
+
+        
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
