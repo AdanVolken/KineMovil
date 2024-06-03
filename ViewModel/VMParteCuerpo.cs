9c7046ed73612733a6fa60f2bd64dda3a74ddf8a
AdanVolken <rolfivolken9@gmail.com>
2024-06-03 18:42:56 -0300

    Create ParteCuerpo ViewModel

---
+++ ViewModel/VMParteCuerpo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KineMovil.ViewModel
+{
+    internal class VMParteCuerpo : INotifyPropertyChanged
+    {
+
+        private string _nombreParte;
+
+        public string NombreParte
+        {
+            get { return _nombreParte; }
+            set { _nombreParte = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
