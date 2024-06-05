0a59b037a36a40962a7d6a5c0d65527839243377
AdanVolken <rolfivolken9@gmail.com>
2024-06-05 11:42:28 -0300

    Create Views

---
+++ View/PaginaSintomaView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KineMovil.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PaginaSintomaView : ContentPage
+	{
+		public PaginaSintomaView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
